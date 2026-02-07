c480223fc1d69d67d0a8fa49c7dc43b83e4e5497
Shawn Schulz <shawnschulz@berkeley.edu>
2026-02-07 13:52:48 -0800

    Run ruff format and linter

--- prototypes/basic_restapi/app.py
+++ prototypes/basic_restapi/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, jsonify, render_template
 import structlog
-from hpc_regression.runner import load_config, run_from_config
+from hpc_regression.runner import run_from_config
 
 logger = structlog.get_logger()
 app = Flask(__name__)
+
 
 @app.route("/api/run_tests")
 def run_tests_extract_metrics():
     outputs = run_from_config("../hpc-runner/configs/runners.yaml")
     return jsonify(outputs)
+
 
 # Return the template html
 @app.route("/")
